f09d73e46dafdbf09aeb739c7eb3dde28c1bc64c
DiegoMarcato <dieguinho-marcato@hotmail.com>
2025-03-06 16:48:23 -0300

    testando

--- 00-java/src/App.java
+++ 00-java/src/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual seu nome?");
+        String name = sc.nextLine();
+        System.out.println("Olá " + name);
+
+        System.out.println("Quantos anos você tem?");
+        int age = sc.nextInt();
+        System.out.println("Você tem " + age);
+        sc.close();
     }
 }
