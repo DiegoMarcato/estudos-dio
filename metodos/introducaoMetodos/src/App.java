bddd53bd53e9bfa5f18bf20736b0efa74b0db013
DiegoMarcato <dieguinho-marcato@hotmail.com>
2025-03-15 17:52:20 -0300

    exs

---
+++ metodos/introducaoMetodos/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args){
+        
+        
+    }
+    public static void soma(double a, double b){
+            
+        double resultado = a + b;
+        System.out.println("Soma: " + resultado);
+    }
+
+    
+}
---
+++ metodos/introducaoMetodos/src/TataW.java
@@ -0,0 +1,7 @@
+public class TataW {
+    public static void main(String[] args) {
+        App.soma(3,5);
+
+        Mensagem.obterMensagem(17);
+    }
+}
